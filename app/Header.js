269a73edc9b3bea15c3d3ee6be2c69aea42bdecb
karshdev <sharmaaakarsh332@gmail.com>
2023-10-10 12:41:36 +0530

    navbar completed

--- app/Header.js
+++ app/Header.js
@@ -1,10 +1,12 @@
+import Navigation from '@/components/Navigation'
 import React from 'react'
 
 const Header = () => {
   return (
     <div className="position relative w-[100vw] h-[100vh] bg-gradient-to-tr from-blue-100 to-blue-100">
-      <img  src='/Cloud.png' className='position absolute w-[700px] h-[200px] top-[10%]  invert grayscale-100 contrast-100'/>
-      <img  src='/Cloud2.png' className='position absolute w-[700px] h-[200px] right-0   invert grayscale-100 contrast-100'/>
+      <Navigation />
+      <img  src='/Cloud.png' className='position absolute w-[700px] h-[200px] top-[10%] opacity-100 invert grayscale-100 contrast-100'/>
+      <img  src='/Cloud2.png' className='position absolute w-[700px] h-[200px] right-0 top-[20%] opacity-100 invert grayscale-100 contrast-100'/>
     </div>
   )
 }
---
+++ components/Navigation.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className='flex p-4 justify-between items-center' >
+      <div className='flex gap-20 items-center justify-center ml-[100px] '>
+     <div>
+      <img src='/LOGO.png' className='w-[98px] h-[92px]'/>
+     </div>
+     
+<div className='flex gap-14 '>
+  <span className="text-gray-600 text-base font-[400px] font-['Manrope'] leading-[14px]">Home</span>
+  <span className="text-gray-600 text-base font-[400px] font-['Manrope'] leading-[14px]">About US</span>
+  </div>  
+  </div>
+  <div className='flex gap-10 mr-[100px] '>
+    <div className="w-[178px] h-10 px-[15px] py-[13px] bg-gray-600 border border-white justify-center items-center gap-2 inline-flex">
+      <img src='download.svg' />
+     <a
+          className=""
+          href=""
+          download
+       >
+        Download
+       </a>
+       </div>
+       <div className='w-[119px] h-10 px-1 py-[9px] justify-start items-center gap-[13px] inline-flex'>
+        <img src='/language.svg' />
+        <div className='flex gap-3 items-center justify-center'>
+          <span className="text-white text-sm font-semibold font-['Manrope'] leading-[14px]">English</span>
+          <img src='/down.svg' />
+        </div>
+       </div>
+  </div>
+
+    </div>
+  )
+}
+
+export default Navigation
