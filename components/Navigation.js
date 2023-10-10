bed6f1d715ea8bca9dcbacf09da28765e8a257c6
karshdev <sharmaaakarsh332@gmail.com>
2023-10-10 14:10:15 +0530

    added responsiveness to navbar

--- components/Navigation.js
+++ components/Navigation.js
@@ -4,33 +4,33 @@ import React from 'react'
 const Navigation = () => {
   return (
     <div className='flex p-4 justify-between items-center' >
-      <div className='flex gap-20 items-center justify-center ml-[100px] '>
+      <div className='flex justify-center items-center gap-20 md:gap-15'>
      <div>
-      <img src='/LOGO.png' className='w-[98px] h-[92px]'/>
+      <img src='/LOGO.png' className='w-[80px] h-[72px] sm:w-[98px] sm:h-[92px]'/>
      </div>
      
-<div className='flex gap-14 '>
-  <Link href="/" className="text-gray-600 text-base font-[400px] font-['Manrope'] leading-[14px]">Home</Link>
-  <Link  href="/" className="text-gray-600 text-base font-[400px] font-['Manrope'] leading-[14px]">About US</Link>
+<div className=' hidden sm:flex sm:gap-14 sm:visible  '>
+  <Link href="/" className="text-gray-600 text-base font-[http://localhost:3000/Cloud2.png400px] font-['Manrope'] leading-[14px]">Home</Link>
+  <Link  href="/" className="text-gray-600 text-base font-[400px] font-['Manrope'] leading-[14px]">About Us</Link>
   </div>  
   </div>
-  <div className='flex gap-10 mr-[100px] '>
-    <div className="w-[190px] h-10 px-[15px] py-[13px] bg-gray-600 border border-white justify-center items-center gap-[10px] inline-flex">
+  <div className='flex gap-10 '>
+    <div className="w-[max-content] h-10 px-[15px] py-[13px] bg-gray-600 border border-white justify-center items-center gap-[10px] inline-flex">
       <img src='download.svg' />
      <a
-          className="text-white text-sm font-[400px]font-['Manrope'] leading-[14px] "
+          className="text-white text-sm font-[400px] font-['Manrope'] leading-[14px]"
           href="/brochure.pdf"
           download
        >
-        Download Brochure
+    Download Brochure
        </a>
        </div>
        <div className='w-[119px] h-10 px-1 py-[9px] justify-start items-center gap-[13px] inline-flex'>
         <img src='/language.svg' />
-        <div className='flex gap-3 items-center justify-center'>
+        <button className='flex gap-3 items-center justify-center'>
           <span className="text-gray-600 text-base font-[400px] font-['Manrope'] leading-[14px] cursor-pointer">English</span>
           <img src='/down.svg' />
-        </div>
+        </button>
        </div>
   </div>
 
