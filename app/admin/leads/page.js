b4cae33d365f3fb1e83b6ecd731059647cb47608
karshdev <sharmaaakarsh332@gmail.com>
2023-10-13 21:57:25 +0530

    added table and dummyData

--- app/admin/leads/page.js
+++ app/admin/leads/page.js
@@ -1,6 +1,6 @@
 import AdminNavbar from '@/components/AdminNavbar'
 import React from 'react'
-
+import dummyData from '@/dummy/dummy'
 
 const Leads = () => {
   return (
@@ -12,45 +12,40 @@ const Leads = () => {
       <input type='date'  className='p-4 border-gray-300 border-2 focus:outline-none  rounded-lg' placeholder='write'/>
       </div>
       
-       <table className="w-full border-separate border-spacing-3">
+      <table className="min-w-full bg-white border border-gray-300 shadow-md">
         <thead>
-          <tr className="text-left">
-            <th className="hidden md:block">Order ID</th>
-            <th>Date</th>
-            <th>Price</th>
-            <th className="hidden md:block">Products</th>
-            <th>Status</th>
+          <tr>
+            <th className="py-2 px-4 border-b">ID</th>
+            <th className="py-2 px-4 border-b">Full Name</th>
+            <th className="py-2 px-4 border-b">Email</th>
+            <th className="py-2 px-4 border-b">Phone</th>
+            <th className="py-2 px-4 border-b">Address</th>
+            <th className="py-2 px-4 border-b">Status</th>
+            <th className="py-2 px-4 border-b">Actions</th>
+            
           </tr>
         </thead>
         <tbody>
+          {/* Sample Row */}
+          {dummyData.map((vlaue)=>(
+             <tr className="hover:bg-gray-100" key={vlaue.id}>
+             <td className="py-2 px-4 border-b text-center">{vlaue.id}</td>
+             <td className="py-2 px-4 border-b text-center">{vlaue.fullName}</td>
+             <td className="py-2 px-4 border-b text-center">{vlaue.email}</td>
+             <td className="py-2 px-4 border-b text-center">{vlaue.phone}</td>
+             <td className="py-2 px-4 border-b text-center">{vlaue.address}</td>
+             <td className="py-2 px-4 border-b text-center">Edited</td>
+             <td className="py-2 px-4 border-b text-center">
+               <button className='underline text-blue-400'>
+                 View
+               </button>
+             </td>
+           </tr>
+          ))
+         }
         
-          <tr className="text-sm md:text-base bg-red-50">
-            <td className="hidden md:block py-6 px-1">sadsad</td>
-            <td className="py-6 px-1">asdas</td>
-            <td className="py-6 px-1">sad</td>
-            <td className="hidden md:block py-6 px-1">asdas</td>
-          
-                <td>
-                  <form
-                    className="flex items-center justify-center gap-4"
-                 
-                  >
-                    <input
-                      placeholder=''
-                      className="p-2 ring-1 ring-red-100 rounded-md"
-                    />
-                    <button className="bg-red-400 p-2 rounded-full">
-                     
-                    </button>
-                  </form>
-                </td>
-          
-                <td className="py-6 px-1">asdas</td>
-             
-          </tr>
-  
-      
-          
+
+          {/* Add more rows as needed */}
         </tbody>
       </table>
        </div>
---
+++ dummy/dummy.js
@@ -0,0 +1,75 @@
+const dummyData = [
+    {
+      id: 1,
+      fullName: 'John Doe',
+      email: 'john.doe@example.com',
+      phone: '555-1234',
+      address: '123 Main St, Cityville',
+    },
+    {
+      id: 2,
+      fullName: 'Jane Smith',
+      email: 'jane.smith@example.com',
+      phone: '555-5678',
+      address: '456 Oak St, Townsville',
+    },
+    {
+      id: 3,
+      fullName: 'Bob Johnson',
+      email: 'bob.johnson@example.com',
+      phone: '555-9876',
+      address: '789 Pine St, Villagetown',
+    },
+    {
+      id: 4,
+      fullName: 'Alice Williams',
+      email: 'alice.williams@example.com',
+      phone: '555-4321',
+      address: '101 Elm St, Hamletville',
+    },
+    {
+      id: 5,
+      fullName: 'Charlie Brown',
+      email: 'charlie.brown@example.com',
+      phone: '555-8765',
+      address: '202 Birch St, Suburbia',
+    },
+    {
+      id: 6,
+      fullName: 'Eva Davis',
+      email: 'eva.davis@example.com',
+      phone: '555-5678',
+      address: '303 Cedar St, Countryside',
+    },
+    {
+      id: 7,
+      fullName: 'David White',
+      email: 'david.white@example.com',
+      phone: '555-4321',
+      address: '404 Maple St, Riverside',
+    },
+    {
+      id: 8,
+      fullName: 'Grace Miller',
+      email: 'grace.miller@example.com',
+      phone: '555-8765',
+      address: '505 Pine St, Mountainside',
+    },
+    {
+      id: 9,
+      fullName: 'Frank Brown',
+      email: 'frank.brown@example.com',
+      phone: '555-1234',
+      address: '606 Oak St, Lakeshore',
+    },
+    {
+      id: 10,
+      fullName: 'Helen Taylor',
+      email: 'helen.taylor@example.com',
+      phone: '555-9876',
+      address: '707 Elm St, Seaside',
+    },
+  ];
+  
+  export default dummyData;
+  
