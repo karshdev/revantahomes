b85fb7dd9a5336ab81bc5a60829c0dbe709454ca
karshdev <sharmaaakarsh332@gmail.com>
2023-10-16 18:21:50 +0530

    removed address optional:true

--- app/api/addHouse/route.js
+++ app/api/addHouse/route.js
@@ -11,7 +11,6 @@ export async function POST(req) {
             fullName,
             email,
             phone,
-           address:"",
             type,
         })
         return NextResponse.json({ message: "House Selected" }, { status: 200 })
--- app/api/addmessage/route.js
+++ app/api/addmessage/route.js
@@ -11,7 +11,7 @@ export async function POST(req) {
             fullName,
             email,
             phone,
-           address:"",
+         
             type:"Contact",
             message
         })
