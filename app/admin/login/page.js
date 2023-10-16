bd60917496f7ce6e48f4e8a696c95a645904bfcb
karshdev <sharmaaakarsh332@gmail.com>
2023-10-16 22:51:01 +0530

    login page design

--- app/admin/login/page.js
+++ app/admin/login/page.js
@@ -1,8 +1,9 @@
 
 "use client"
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 import {  signIn,  } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 
 const LoginPage = () => {
@@ -33,40 +34,62 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-100 via-blue-300 to-blue-400">
-
-    <div className="bg-white p-8 rounded shadow-md w-96">
-      <h1 className="text-3xl font-bold mb-4 text-center text-gray-800">Login Page</h1>
-      <form onSubmit={handleSubmit}>
-        <label className="block mb-4">
-          <span className="text-gray-700">Username:</span>
+    <div className="min-h-screen relative bg-[url('/explore_images/explore_bg.png')] bg-brand w-full bg-blend-multiply py-12 flex items-center justify-center">
+    <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
+      <h1 className="text-3xl font-bold text-center text-gray-700">LOGIN</h1>
+      <form className="mt-6" onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-sm font-semibold text-gray-800"
+            
+          >
+            Email
+          </label>
           <input
-            type="text"
-            className="mt-1 p-2 w-full rounded border focus:outline-none focus:ring focus:border-blue-300"
+            type="email"
+            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
             value={credentials.email}
             onChange={(e) => setCredentials({ ...credentials, email: e.target.value })}
           />
-        </label>
-        <label className="block mb-4">
-          <span className="text-gray-700">Password:</span>
+        </div>
+        <div className="mb-2">
+          <label
+            htmlFor="password"
+            className="block text-sm font-semibold text-gray-800"
+          >
+            Password
+          </label>
           <input
             type="password"
-            className="mt-1 p-2 w-full rounded border focus:outline-none focus:ring focus:border-blue-300"
+            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
             value={credentials.password}
             onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
           />
-        </label>
-
-        <div className='flex gap-4  items-center justify-between'>
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-        >
-          Sign In
-        </button>
-        {error && <span className="text-sm font-bold text-center text-red-400">Incorrect Email/Password.</span>}
         </div>
+        <Link
+            href=""
+            className="text-xs text-blue-600 hover:underline"
+          >
+            Forget Password?
+          </Link>
+        <div className="mt-2">
+          <button type='submit' className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+            Login
+          </button>
+        </div>
+        <p className="mt-4 text-sm text-center text-gray-700">
+          Don't have an account?{" "}
+          <Link
+            href=""
+            className="font-medium text-blue-600 hover:underline"
+          >
+            Sign up
+          </Link>
+        </p>
       </form>
+      {error &&  <span className=" text-red-500 w-[100%] text-center  px-4 py-2 rounded-md inline-block">Wrong Email/Paswword</span>}
+
     </div>
   </div>
   );
